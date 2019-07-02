35ad9f3a5fde3a845bad57a74506d7acbb2cf50d
Ben Smith <bh2smith@gmail.com>
2019-07-02 11:19:47 +0200

    exhaustive unit tests

--- test/test_multi.js
+++ test/test_multi.js
@@ -1,20 +1,61 @@
 const MultiCaller = artifacts.require("MultiCaller")
 const DummyContract = artifacts.require("DummyContract")
 
+const truffleAssert = require("truffle-assertions")
+
 contract("MultiCaller", async (accounts) => {
 
-  describe("No attribute multi-call", () => {
+
+  describe("executeWithFunctionName()", () => {
     
-    it("execute()", async () => {
+    it("should call function without arguments by name repeatedly", async () => {
       const dummy = await DummyContract.new()
       const multiCaller = await MultiCaller.new()
 
-      const valueBefore = (await dummy.value.call()).toNumber()
       const numCalls = 100
-      await multiCaller.execute(dummy.address, numCalls, "incrementValue()")
+      await multiCaller.executeWithFunctionName(dummy.address, numCalls, "incrementValue()")
       const valueAfter = (await dummy.value.call()).toNumber()
 
-      assert.equal(valueAfter, valueBefore + numCalls, "Something went wrong with value increment!")
+      assert.equal(valueAfter, numCalls, "Expected value should be numCalls")
+    })
+
+    it("should fail when calling non-existent function", async () => {
+      const dummy = await DummyContract.new()
+      const multiCaller = await MultiCaller.new()
+
+      await truffleAssert.reverts(
+        multiCaller.executeWithFunctionName(dummy.address, 1, "nonExistantFunction()"), 
+        "One or more of the transactions failed!"
+      )
+    })
+
+    it("should fail when calling function that reverts", async () => {
+      const dummy = await DummyContract.new()
+      const multiCaller = await MultiCaller.new()
+
+      await truffleAssert.reverts(
+        multiCaller.executeWithFunctionName(dummy.address, 1, "alwaysRevert()"), 
+        "One or more of the transactions failed!"
+      )
+    })
+  })
+
+  describe("executeWithCalldata()", () => {
+    it("should call function with arguments repeatedly", async () => {
+      const dummy = await DummyContract.new()
+      const multiCaller = await MultiCaller.new()
+
+      const numCalls = 100
+      const increment = 42
+      const calldata = dummy.contract.methods.incrementBy(increment).encodeABI()
+
+      await multiCaller.executeWithCalldata(dummy.address, numCalls, calldata)
+      const valueAfter = (await dummy.value.call()).toNumber()
+
+      assert.equal(
+        valueAfter, numCalls * increment, 
+        "Expected value should be increment * numCalls"
+      )
     })
   })
 })
